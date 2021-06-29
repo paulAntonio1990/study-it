4aa2c923a04914ed9145afd83692fba2231d8a75
Paul <paulantonioifrmi@yahoo.com>
2021-06-29 09:34:46 +0300

    added filtering and ordering for courses

--- src/app/components/courses-dashboard/courses-dashboard.component.ts
+++ src/app/components/courses-dashboard/courses-dashboard.component.ts
@@ -1,8 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CourseDto} from "../../domain/courseDto";
 import {CourseService} from "../../services/course.service";
 import {MatDialog} from "@angular/material/dialog";
 import {AddCourseDialogComponent} from "./add-course-dialog/add-course-dialog.component";
+import {OrderingType} from "../../domain/orderingType";
+import {MatSelectChange} from "@angular/material/select";
+import {CourseDomain} from "../../domain/courseDomain";
+import {StudyType} from "../../domain/studyType";
+import {StudyYear} from "../../domain/studyYear";
 
 @Component({
   selector: 'app-courses-dashboard',
@@ -12,10 +17,17 @@ import {AddCourseDialogComponent} from "./add-course-dialog/add-course-dialog.co
 export class CoursesDashboardComponent implements OnInit {
 
   courses: CourseDto[] = [];
+  tmpCourses: CourseDto[] = [];
   courseNameFilter!: string;
+  orderingTypes = [OrderingType.A_Z, OrderingType.Z_A];
+  courseDomains = [CourseDomain.INFORMATICA, CourseDomain.STATISTICA, CourseDomain.ECONOMIE];
+  studyPrograms = [StudyType.LICENTA, StudyType.MASTERAT, StudyType.DOCTORAT];
+  studyYears = [StudyYear.I, StudyYear.II, StudyYear.III];
+  filters: any[] = [];
 
   constructor(private readonly courseService: CourseService,
-              public dialog: MatDialog) { }
+              public dialog: MatDialog) {
+  }
 
   ngOnInit(): void {
     this.getCourses();
@@ -23,7 +35,10 @@ export class CoursesDashboardComponent implements OnInit {
 
   getCourses() {
     this.courseService.getCourses()
-      .subscribe(courses => this.courses = courses);
+      .subscribe(courses => {
+        this.tmpCourses = courses;
+        this.courses = this.tmpCourses;
+      });
   }
 
   addCourse() {
@@ -39,5 +54,51 @@ export class CoursesDashboardComponent implements OnInit {
     dialogRef.afterClosed().subscribe(() => {
       window.location.reload();
     })
+  }
+
+  onOrderingFilterChange($event: MatSelectChange) {
+    this.orderByFilter($event.value);
+  }
+
+  private orderByFilter(value: any) {
+    switch (value) {
+      case OrderingType.A_Z:
+        this.courses = this.courses.sort((first, second) => this.orderAscending(first.name, second.name));
+        break;
+      case OrderingType.Z_A:
+        this.courses = this.courses.sort((first, second) => (-1) * this.orderAscending(first.name, second.name));
+        break;
+    }
+  }
+
+  private orderAscending(first: string, second: string) {
+    if (first.toLowerCase() <= second.toLowerCase()) {
+      return -1;
+    } else {
+      return 1;
+    }
+  }
+
+  onFilterChange($event: MatSelectChange) {
+    console.log($event);
+    this.generateFilter($event);
+
+    this.courses = this.tmpCourses.filter((c: any) => this.filters.every(filter => c[filter.type] === filter.name));
+  }
+
+  private generateFilter($event: MatSelectChange) {
+    if ($event.value) {
+      const foundIndex = this.filters.findIndex(filter => filter.type === $event.value.type);
+      if (foundIndex === -1) {
+        this.filters.push($event.value);
+      } else {
+          this.filters[foundIndex] = $event.value;
+      }
+    } else {
+      const foundIndex = this.filters.findIndex(filter => filter.type === $event.source.options.last.value.type);
+      if (foundIndex !== -1) {
+        this.filters.splice(foundIndex, 1)
+      }
+    }
   }
 }
---
+++ src/app/domain/orderingType.ts
@@ -0,0 +1,4 @@
+export enum OrderingType {
+  A_Z= 'A-Z',
+  Z_A = 'Z-A'
+}
